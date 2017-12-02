db6e41ad6b18a78093b08611496b667cb3e53a5f
mmaron96 <matthewbm628@gmail.com>
2017-12-02 09:14:02 -0600

    Got the Cannon Code imported

---
+++ Assets/Scripts/Cannonrotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonrotation : MonoBehaviour {
+    public float rotationSpeed = 3f;
+
+   // private SpriteRenderer theSpriteRenderer;
+
+    // Use this for initialization
+    void Start () {
+       // theSpriteRenderer = GetComponent<>
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Vertical");
+        print(transform.rotation.z);
+
+        if(inputX > 0 && transform.rotation.z <= 57.952/120)
+        {
+            transform.Rotate(Vector3.forward, inputX * rotationSpeed);
+        }
+        else if(inputX < 0 && transform.rotation.z >= -38.535/120)
+        {
+            transform.Rotate(Vector3.forward, inputX * rotationSpeed);
+        }
+
+
+
+    }
+}
